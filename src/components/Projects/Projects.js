97ea9fe1937080e5c830a3f7da3d2d2b992cc6e8
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2023-06-05 15:49:32 +0200

    mobile web tab bar added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,40 @@
+import React, { useState } from "react";
 import MobileProjects from "./MobileProjects";
 import WebProjects from "./WebProjects";
+import H2 from "../UI/H2/H2";
+import H3 from "../UI/H3/H3";
+import classes from "./Projects.module.css";
 
 const Projects = () => {
+  const [isMobile, setIsMobile] = useState(true);
+
   return (
     <div>
-      <WebProjects></WebProjects>
-      {/* <MobileProjects /> */}
+      <div className={classes["projects-headline-container"]}>
+        <div className={classes["tab-bar-container"]}>
+          <H2 text={"PROJECTS"} />
+          <div
+            className={`${classes["mobile-tab"]} ${
+              isMobile ? classes["selected"] : classes["unSelected"]
+            }`}
+            onClick={() => setIsMobile(true)}
+          >
+            <H3
+              text={"Mobile"}
+              color={`${isMobile ? "" : "rgba(0,0,0,0.4)"}`}
+            />
+          </div>
+          <div
+            className={`${classes["web-tab"]} ${
+              isMobile ? classes["unSelected"] : classes["selected"]
+            }`}
+            onClick={() => setIsMobile(false)}
+          >
+            <H3 text={"Web"} color={`${isMobile ? "rgba(0,0,0,0.4)" : ""}`} />
+          </div>
+        </div>
+      </div>
+      {isMobile ? <MobileProjects /> : <WebProjects />}
     </div>
   );
 };
--- src/components/UI/H3/H3.js
+++ src/components/UI/H3/H3.js
@@ -2,7 +2,7 @@ const H3 = (props) => {
   return (
     <h3
       style={{
-        color: "#303030",
+        color: `${props.color || "#303030"}`,
         fontSize: "4.8rem",
         lineHeight: "1.2",
         fontWeight: "500",
